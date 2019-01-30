a5459cdd3fa43d04c5821cdb954f50cc12727a16
Naimur Rahman <naimur978@gmail.com>
2019-01-30 12:38:20 +0600

    Add files via upload

---
+++ Let's See/javascript/profile.js
@@ -0,0 +1,10 @@
+function h(e) {
+  $(e).css({'height':'65px','overflow-y':'hidden'}).height(e.scrollHeight-30);
+}
+$('textarea').each(function () {
+  h(this);
+}).on('input', function () {
+  h(this);
+});
+
+
